80231906299db706009125968a39ce4e04cf63fb
a17759504 <Samoylov.s.g@sberbank.ru>
2020-12-20 15:21:43 +0300

    adding kubernetes-network homework

---
+++ kubernetes-networks/metallb-scripts.sh
@@ -0,0 +1,7 @@
+# Installing MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/baremetal/deploy.yaml
+
